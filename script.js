5c418592ba37cc1fe3f4c4b4fa3fbbd9b163a522
Luiz Gustavo <luizgustavonovaes01@gmail.com>
2026-06-15 17:12:14 -0400

    add goalChart implementation with doughnut chart configuration

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    new Chart(
+        document.getElementById("goalChart"),
+        {
+            type: "doughnut",
+            data: {
+                datasets: [{
+                    data: [84, 16],
+                    backgroundColor: [
+                        "#F6C45A", // amarelo
+                        "#F3EBDD"  // fundo
+                    ],
+                    borderWidth: 0,
+                    borderRadius: 20
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+
+                rotation: -90, // começa na esquerda
+                circumference: 180, // meia lua
+
+                cutout: "80%",
+
+                plugins: {
+                    legend: {
+                        display: false
+                    },
+                    tooltip: {
+                        enabled: false
+                    }
+                }
+            }
+        }
+    );
+
+});
